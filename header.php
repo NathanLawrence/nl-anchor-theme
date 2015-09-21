0fdd57520b256c5db3b5699a5fdff0bed9b2674a
Nathan Lawrence <nathan@nathanlawrence.org>
2015-09-21 01:00:22 -0500

    CMS Citation.

--- header.php
+++ header.php
@@ -42,6 +42,9 @@
 				        </ul>
 				    </nav>
 				<?php endif; ?>
+				<div id="navFooter">
+					<div class="anchorCitation">Proud member of the <a href="http://anchorcms.com/">Anchor CMS</a> community. Fork my theme at <a href="https://github.com/NathanLawrence/nl-anchor-theme">GitHub</a>.</div>
+				</div>
 			</div>
 		</header>
 
