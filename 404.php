8de3df62610cfed4119ed6620a1cc73a7a5059e5
Nathan Lawrence <nathan@nathanlawrence.org>
2015-09-20 20:36:28 -0500

    Complete reset to skeleton
    After a while of working, it became clear that the right strategy here
    was to completely start over and write everything myself. This is the
    Anchor skeleton that all commits will be based on from now on.

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php theme_include('header'); ?>
-
-	<section class="content wrap">
-		<h1 class="404-head">Looks like I made a mistake.</h1>
-
-		<p>Sorry about this, but the page <code>/<?php echo current_url(); ?></code> could not be found. Your best bet is either to try the <a href="<?php echo base_url(); ?>">homepage</a>, try <a href="#search">searching</a>, or go and cry in a corner (although I don’t recommend the latter).</p>
-	</section>
-
-<?php theme_include('footer'); ?>
--- article.php
+++ article.php
@@ -1,63 +1,33 @@
 <?php theme_include('header'); ?>
 
-		<section class="content wrap" id="article-<?php echo article_id(); ?>">
-			<h1><?php echo article_title(); ?></h1>
-
+		<section class="content">
 			<article>
-				<?php echo article_markdown(); ?>
+		    	<h1><?php echo article_title(); ?></h1>
+		        <h5><?php echo article_date(); ?></h5>
+			    <?php echo article_markdown(); ?>
+			    <?php echo article_custom_field('attribution'); ?>
 			</article>
-
-			<section class="footnote">
-				<!-- Unfortunately, CSS means everything's got to be inline. -->
-				<p>This article is my <?php echo numeral(total_articles()); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long<?php if(comments_open()): ?>, and it’s got <?php echo total_comments() . pluralise(total_comments(), ' comment'); ?> for now.<?php endif; ?> <?php echo article_custom_field('attribution'); ?></p>
-			</section>
 		</section>
-
-		<?php if(comments_open()): ?>
+	
 		<section class="comments">
-			<?php if(has_comments()): ?>
-			<ul class="commentlist">
-				<?php $i = 0; while(comments()): $i++; ?>
-				<li class="comment" id="comment-<?php echo comment_id(); ?>">
-					<div class="wrap">
-						<h2><?php echo comment_name(); ?></h2>
-						<time><?php echo relative_time(comment_time()); ?></time>
-
-						<div class="content">
-							<?php echo comment_text(); ?>
-						</div>
-
-						<span class="counter"><?php echo $i; ?></span>
-					</div>
-				</li>
-				<?php endwhile; ?>
-			</ul>
+			<!-- Comment List -->
+			<?php if(comments_open() and has_comments()): ?>
+			    <?php while(comments()): ?>
+			        <?php echo comment_text(); ?>
+			        <?php echo comment_date(); ?>
+			    <?php endwhile; ?>
 			<?php endif; ?>
-
-			<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
-				<?php echo comment_form_notifications(); ?>
-
-				<p class="name">
-					<label for="name">Your name:</label>
-					<?php echo comment_form_input_name('placeholder="Your name"'); ?>
-				</p>
-
-				<p class="email">
-					<label for="email">Your email address:</label>
-					<?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
-				</p>
-
-				<p class="textarea">
-					<label for="text">Your comment:</label>
+			
+			<!-- Comment Form -->
+			<?php if(comments_open()): ?>
+				<form id="comment" method="post" action="<?php echo comment_form_url(); ?>#comment">
+					<?php echo comment_form_notifications(); ?><!-- Error notifications -->
+					<?php echo comment_form_input_name('placeholder="Your Name"'); ?>
+					<?php echo comment_form_input_email('placeholder="Your Email"', $extra = ''); ?>
 					<?php echo comment_form_input_text('placeholder="Your comment"'); ?>
-				</p>
-
-				<p class="submit">
-					<?php echo comment_form_button(); ?>
-				</p>
-			</form>
-
+					<?php echo comment_form_button($text = 'Post Comment', $extra = ''); ?>
+				</form>
+			<?php endif; ?>
 		</section>
-		<?php endif; ?>
 
 <?php theme_include('footer'); ?>
