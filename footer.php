dea5f221f6d900785ae29504f87b04f988410ce7
Nathan Lawrence <nathan@nathanlawrence.org>
2015-10-11 00:33:16 -0500

    Moved the footer to where it belongs, in the footer.php file.

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 		<footer>
+				<div id="footerFinePrint">
+					<div class="anchorCitation">Proud member of the <a href="http://anchorcms.com/">Anchor CMS</a> community. Fork my theme at <a href="https://github.com/NathanLawrence/nl-anchor-theme">GitHub</a>.</div>
+				</div>
 		</footer>
 	</body>
 </html>
