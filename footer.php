9e4bfb20f4b1f1be33f5d1d7fbf5efee69ecabf3
Nathan Lawrence <nathan@nathanlawrence.org>
2015-10-11 15:37:45 -0500

    put the fine print in its own BS row

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 		<!--Start Footer-->
 		<footer>
-				<div id="footerFinePrint">
-					<div class="anchorCitation">Proud member of the <a href="http://anchorcms.com/">Anchor CMS</a> community. Fork my theme at <a href="https://github.com/NathanLawrence/nl-anchor-theme">GitHub</a>.</div>
+				<div class="row">
+					<div id="footerFinePrint">
+						<div class="anchorCitation">Proud member of the <a href="http://anchorcms.com/">Anchor CMS</a> community. Fork my theme at <a href="https://github.com/NathanLawrence/nl-anchor-theme">GitHub</a>.</div>
+					</div>
 				</div>
 		</footer>
 		<!--End div class of container-->
