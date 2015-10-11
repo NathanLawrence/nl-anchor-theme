7fc8cb36911b94c331d373f5e2a2a7fe9a6461db
Nathan Lawrence <nathan@nathanlawrence.org>
2015-10-11 15:11:31 -0500

    Structuring homepage

--- posts.php
+++ posts.php
@@ -1,17 +1,23 @@
 <?php theme_include('header'); ?>
-
-		<section class="content">
-			<?php if(has_posts()) : while(posts()) : ?>
-				<article>
-			    	<h1>
-			        	<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
-			        </h1>
-			        <h5><?php echo article_date(); ?></h5>
-					<p><?php echo article_description(); ?></p>
-				</article>
-			<?php endwhile; endif; ?>
-		</section>
-		
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-6">
+				<section class="content">
+					<?php if(has_posts()) : while(posts()) : ?>
+						<article>
+					    	<h1>
+					        	<a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
+					        </h1>
+					        <h5><?php echo article_date(); ?></h5>
+							<p><?php echo article_description(); ?></p>
+						</article>
+					<?php endwhile; endif; ?>
+				</section>
+			</div>
+			<div class="col-md-2" id="bio-gutter">
+				<h1>About Me</h1>
+			</div>
+		</div>
 		<section class="pagination">
 			<?php if(has_pagination()) : ?>
 			    <nav class="pagination">
