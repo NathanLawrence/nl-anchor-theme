51e6fd55f185fac136212922ed5374622d48f82b
Nathan Lawrence <nathan@nathanlawrence.org>
2015-10-11 01:09:07 -0500

    Moving container as an experiment.

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-		<!--End div class of container-->
-		</div>
 		<!--Start Footer-->
 		<footer>
 				<div id="footerFinePrint">
 					<div class="anchorCitation">Proud member of the <a href="http://anchorcms.com/">Anchor CMS</a> community. Fork my theme at <a href="https://github.com/NathanLawrence/nl-anchor-theme">GitHub</a>.</div>
 				</div>
 		</footer>
+		<!--End div class of container-->
+		</div>
 	</body>
 </html>
