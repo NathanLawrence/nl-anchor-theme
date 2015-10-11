c2e31a48c0916e76560baac39a05fbda245df5d4
Nathan Lawrence <nathan@nathanlawrence.org>
2015-10-11 01:01:54 -0500

    Commenting is God's work.

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-		</div> //end Div with class of container
+		<!--End div class of container-->
+		</div>
+		<!--Start Footer-->
 		<footer>
 				<div id="footerFinePrint">
 					<div class="anchorCitation">Proud member of the <a href="http://anchorcms.com/">Anchor CMS</a> community. Fork my theme at <a href="https://github.com/NathanLawrence/nl-anchor-theme">GitHub</a>.</div>
