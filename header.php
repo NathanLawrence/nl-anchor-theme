29b032802feb08a733dbbd562f608d8ddcdb81a3
Nathan Lawrence <nathan@nathanlawrence.org>
2015-10-11 20:20:32 -0500

    Added GA tracking code.

--- header.php
+++ header.php
@@ -19,6 +19,16 @@
 		
 		<link rel="shortcut icon" href="<?php echo theme_url('img/favicon.ico'); ?>" type="image/x-icon" />
 		
+		<!--Google Analytics Tracking Code-->
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga')	
+			ga('create', 'UA-24079898-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+
 		<!-- Add per-post CSS -->
 		<?php if(article_css()): ?>
 			<style><?php echo article_css(); ?></style>
